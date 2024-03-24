084913fb6753a65ec098c0bcef0c003a91fd9fab
Pablo Junior <pablojuniorgn2@gmail.com>
2024-03-23 23:10:04 -0300

    feat: first commit

---
+++ src/main/java/com/gntech/store/gnstore/GnStoreApplication.java
@@ -0,0 +1,13 @@
+package com.gntech.store.gnstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gntech/store/gnstore/GnStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gntech.store.gnstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
